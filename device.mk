9ee48eb183feabd94dd5093ffce3bed30b42b54e
Michael Wu <mwu@mozilla.com>
2013-07-29 17:36:39 -0400

    Remove librs_jni from PRODUCT_PACKAGES

--- device.mk
+++ device.mk
@@ -158,7 +158,6 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_PACKAGES += \
-	librs_jni \
 	com.android.future.usb.accessory
 
 # Filesystem management tools
